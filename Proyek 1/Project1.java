405c0f255218ddb5d437f9173db4dd074fa3adba
AhmadFadzil <ahmadfadzil214@gmail.com>
2024-08-26 15:20:44 +0700

    Proyek 1

---
+++ Proyek 1/Project1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Ahmad Fadzil\n"
+                + "NPM : 232050047\n"
+                + "Alamat : Sanggrahan, Rejosari, Kec.Pakis, Kab.Magelang\n"
+                + "No.Hp : 087771346980\n");
+                
+    }
+}
---
+++ Proyek 1/fadzil_project1/src/main/java/com/mycompany/fadzil_project1/Fadzil_project.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fadzil_project1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Fadzil_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Ahmad Fadzil\n"
+                + "NPM : 232050047\n"
+                + "Alamat : Sanggrahan, Rejosari, Kec.Pakis, Kab.Magelang\n"
+                + "No.Hp : 087771346980\n");
+    }
+}
