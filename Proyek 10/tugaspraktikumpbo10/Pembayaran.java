75c9f83d219f34cd4f16a16588c14ab558206ec3
AhmadFadzil <ahmadfadzil214@gmail.com>
2024-11-01 19:18:25 +0700

    Praktikum 10

---
+++ Proyek 10/tugaspraktikumpbo10/Pembayaran.java
@@ -0,0 +1,15 @@
+package com.mycompany.tugaspraktikumpbo10;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public interface Pembayaran {
+    double hitungPajak(double harga);
+    
+}
